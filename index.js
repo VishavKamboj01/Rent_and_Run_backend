a17fc10febaa0eaac7930227b04c19ce4ef52e4b
VishavKamboj01 <vishavbehar@gmail.com>
2022-05-29 14:32:59 +0530

    Added Console logging support on unhandelled exceptions.

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ import connectToMongoDB from "./startup/connection.js";
 import logger from "./startup/logging.js";
 const app = express();
 import router from "./startup/routes.js";
+import config from "./startup/config.js";
 
 //Initializing winston for logging
 logger();
+//Configuration of app settings
+config();
 //Initializing all the routes
 router(app);
 
--- startup/logging.js
+++ startup/logging.js
@@ -4,6 +4,16 @@ import "express-async-errors"; // Wraps our routes in try catch block at runtine
 
 export default () => {
   winston.add(
+    new winston.transports.Console({
+      level: "debug",
+      handleExceptions: true,
+      json: false,
+      colorize: true,
+      prettyPrint: true,
+    })
+  );
+
+  winston.add(
     new winston.transports.File({
       filename: "logfile.log",
       handleExceptions: true,
