b62760bdc6192e10a0ca3f8916f9e162c30e44ea
VishavKamboj01 <vishavbehar@gmail.com>
2022-05-29 13:02:00 +0530

    refactored configuration settings into another module.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import config from "config";
 import connectToMongoDB from "./startup/connection.js";
 import logger from "./startup/logging.js";
 const app = express();
@@ -9,9 +8,6 @@ import router from "./startup/routes.js";
 logger();
 //Initializing all the routes
 router(app);
-
-if (!config.get("jwtPrivateKey"))
-  throw new Error("FATEL ERROR: jwtPrivateKey is not set!");
 
 const port = process.env.PORT || 3000;
 app.listen(port, (err) => console.log(`Listening on Port ${port}`));
